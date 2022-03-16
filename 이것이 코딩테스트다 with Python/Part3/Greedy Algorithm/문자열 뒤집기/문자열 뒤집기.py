edd1db301743e684db1f10dafe72baa4f6d1900f
KangukLee-97 <rkddnrdl97@naver.com>
2022-03-16 18:33:42 +0900

    [이취코] 그리디-문자열 뒤집기 풀이 추가

--- 이것이 코딩테스트다 with Python/Part3/Greedy Algorithm/문자열 뒤집기/문자열 뒤집기.py
+++ 이것이 코딩테스트다 with Python/Part3/Greedy Algorithm/문자열 뒤집기/문자열 뒤집기.py
@@ -1,10 +1,13 @@
+# 책의 풀이
 num_str = input()
 first = num_str[0]   # 맨 첫번째 문자
 count0 = 0   # 모두 0으로 바꾸는 경우
 count1 = 0   # 모두 1로 바꾸는 경우
 
-if first == '1': count0 += 1
-else: count1 += 1
+if first == '1':
+    count0 += 1
+else:
+    count1 += 1
 
 # 두 번째 원수부터 모든 원소 확인
 for i in range(len(num_str)-1):
@@ -16,3 +19,13 @@ for i in range(len(num_str)-1):
             count1 += 1
 
 print(min(count0, count1))
+
+# 외부 풀이
+s = input()
+cnt = 0
+
+for i in range(len(s)-1):
+    if s[i] != s[i+1]:
+        cnt += 1
+
+print((cnt+1) // 2)
